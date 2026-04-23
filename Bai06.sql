9df20b58e84e628fbd5a79da27d403afb0535826
Hung D <dongvantienhung456@gmail.com>
2026-04-23 20:03:37 +0700

    IT202-Ss3-Ex06-UpdateYeuCauThaySang

--- Bai06.sql
+++ Bai06.sql
@@ -1,7 +1,11 @@
+CREATE DATABASE TEST;
+USE TEST;
+
+DROP TABLE PRODUCTS;
 CREATE TABLE PRODUCTS (
 	productID VARCHAR(4) PRIMARY KEY,
     prdName varchar(255) NOT NULL,
-    size char(1) NOT NULL,
+    size ENUM ('L','M','XL'),
     price INT
 );
 
@@ -10,15 +14,22 @@ VALUES
 	("P01", "Áo sơ mi trắng", "L", 250000),
 	("P02", "Quần Jean xanh", "M", 450000),
 	("P03", "Áo thun Basic", "XL", 150000),
-	("P04", "Áo hoodie", NULL, -200000);
+	("P04", "Áo hoodie", NULL, -200000),
+    ("P05", "Áo thun ", "XL", 1500000),
+    ("P06", "Áo thun Nike", "L", 30000),
+    ("P07", "Giày Basic", "M", 250000),
+    ("P08", "Quần Nike", "L", 450000),
+    ("P09", "Áo Basic", "XL", 50000),
+    ("P10", "Áo Common","L", 25000);
     
-    
-UPDATE PRODCUTS SET price = 400000
+UPDATE PRODUCTS SET price = 400000
 WHERE productID = "P02";
 
+SET SQL_SAFE_UPDATES = 0;
 UPDATE PRODUCTS SET price = price * 1.1;
 
 DELETE FROM PRODUCTS WHERE productID = "P03";
+DELETE FROM PRODUCTS WHERE size = NULL OR price < 0;
 
 SELECT * FROM PRODUCTS;
 SELECT prdName , size FROM PRODUCTS;
